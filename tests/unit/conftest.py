3a4e49c8e583d81efccb4f62e89972037ec44cab
Bradley A. Thornton <bthornto@redhat.com>
2022-06-08 05:35:11 -0700

    Add a basic smoke unit test (#12)
    * Add unit tests
    
    * Add unit tests
    
    * Changelog
    
    * Fix boilerplate issues
    
    * Add empty requirements.txt file
    
    * Constraint for utils
    
    * Remove dep on lint

---
+++ tests/unit/conftest.py
@@ -0,0 +1,42 @@
+"""Shared fixtures for unit tests."""
+
+from __future__ import absolute_import, division, print_function
+
+
+# pylint: disable=invalid-name
+__metaclass__ = type
+# pylint: enable=invalid-name
+
+import types
+
+from typing import Dict, Union
+
+import pytest
+
+from ansible.parsing.dataloader import DataLoader
+from ansible.playbook.play_context import PlayContext
+from ansible.playbook.task import Task
+from ansible.plugins import loader as Loader
+from ansible.plugins.connection.local import Connection
+from ansible.template import Templar
+
+from .definitions import ActionModuleInit
+
+
+@pytest.fixture
+def action_init() -> ActionModuleInit:
+    """Provide a fixture for action initialization.
+
+    :returns: A dictionary of action initialization arguments.
+    """
+    play_context = PlayContext()
+    loader = DataLoader()
+
+    return {
+        "connection": Connection(play_context=play_context, new_stdin=None),
+        "loader": loader,
+        "play_context": PlayContext(),
+        "shared_loader_obj": Loader,
+        "task": Task(),
+        "templar": Templar(loader=loader),
+    }
---
+++ tests/unit/test_smoke.py
@@ -0,0 +1,46 @@
+"""Some basic smoke tests."""
+
+from __future__ import absolute_import, division, print_function
+
+
+# pylint: disable=invalid-name
+__metaclass__ = type
+# pylint: enable=invalid-name
+
+from typing import Union
+
+import pytest
+
+from ansible.errors import AnsibleActionFail
+from ansible.parsing.dataloader import DataLoader
+from ansible.playbook.play_context import PlayContext
+from ansible.playbook.task import Task
+from ansible.plugins import loader as Loader
+from ansible.plugins.connection.local import Connection
+from ansible.template import Templar
+
+from ...plugins.action.git_away import ActionModule as GitAwayActionModule
+from ...plugins.action.git_here import ActionModule as GitHereActionModule
+from .definitions import ActionModuleInit
+
+
+@pytest.mark.parametrize(
+    "module",
+    (GitHereActionModule, GitAwayActionModule),
+    ids=("git_here", "git_away"),
+)
+def test_fail_argspec(
+    action_init: ActionModuleInit,
+    module: Union[GitAwayActionModule, GitHereActionModule],
+) -> None:
+    """Test an argspec failure.
+
+    :param action_init: A fixture for action initialization.
+    :param module: The module to test.
+    """
+    play_context = PlayContext()
+    loader = DataLoader()
+
+    action = module(**action_init)
+    with pytest.raises(AnsibleActionFail):
+        action.run()
