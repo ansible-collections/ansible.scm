c960893f39fb1c0b37be4635ac7d6abeeaec5b48
Bradley A. Thornton <bthornto@redhat.com>
2023-03-29 11:15:59 -0700

    Add boiler changelog

--- tests/unit/definitions.py
+++ tests/unit/definitions.py
@@ -14,12 +14,11 @@ from typing import Dict, Union
 from ansible.parsing.dataloader import DataLoader
 from ansible.playbook.play_context import PlayContext
 from ansible.playbook.task import Task
-from ansible.plugins import loader as Loader
 from ansible.plugins.connection.local import Connection
 from ansible.template import Templar
 
 
 ActionModuleInit = Dict[
     str,
-    Union[Connection, PlayContext, DataLoader, Loader, Task, types.ModuleType, Templar],
+    Union[Connection, PlayContext, DataLoader, Task, types.ModuleType, Templar],
 ]
