4e48769b1c01c4760569955acdae9515a2c5cc12
Bradley A. Thornton <bthornto@redhat.com>
2023-04-11 05:10:28 -0700

    Lint

--- plugins/action/git_publish.py
+++ plugins/action/git_publish.py
@@ -1,6 +1,7 @@
 # Copyright 2022 Red Hat
 # GNU General Public License v3.0+
 # (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+"""The git_publish action plugin."""
 
 from __future__ import absolute_import, division, print_function
 
