66ec4bc3315b7b5f1a2b11bea3b89aab519cfc53
Bradley A. Thornton <bthornto@redhat.com>
2023-08-07 12:58:59 -0700

    Remove previous integration

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,66 +1,13 @@
-"""Run the integration tests."""
+"""Tests for molecule scenarios."""
 from __future__ import absolute_import, division, print_function
 
-import subprocess
-
-from typing import Dict
-
-import pytest
-
-from pytest_ansible_network_integration import AnsibleProject
+from pytest_ansible.molecule import MoleculeScenario
 
 
-# pylint: disable=invalid-name
-__metaclass__ = type
-# pylint: enable=invalid-name
+def test_integration(molecule_scenario: MoleculeScenario) -> None:
+    """Run molecule for each scenario.
 
-
-def run(localhost_project: AnsibleProject, environment: Dict[str, str]) -> None:
-    """Run the integration tests.
-
-    :param localhost_project: The localhost project.
-    :param environment: The environment.
+    :param molecule_scenario: The molecule scenario object
     """
-    __tracebackhide__ = True  # pylint: disable=unused-variable
-    args = [
-        "ansible-navigator",
-        "run",
-        str(localhost_project.playbook),
-        "--ee",
-        "false",
-        "--mode",
-        "stdout",
-        "--pas",
-        str(localhost_project.playbook_artifact),
-        "--ll",
-        "debug",
-        "--lf",
-        str(localhost_project.log_file),
-        "--cdcp",
-        str(localhost_project.collection_doc_cache),
-        "-vvvv",
-    ]
-    process = subprocess.run(
-        args=args,
-        env=environment,
-        capture_output=True,
-        check=False,
-        shell=False,
-    )
-    if process.returncode:
-        print(process.stdout.decode("utf-8"))
-        print(process.stderr.decode("utf-8"))
-
-        pytest.fail(f"Integration test failed: {localhost_project.role}")
-
-
-def test_integration(
-    localhost_project: AnsibleProject,
-    environment: Dict[str, str],
-) -> None:
-    """Run the integration tests.
-
-    :param localhost_project: The localhost project.
-    :param environment: The environment.
-    """
-    run(localhost_project, environment)
+    proc = molecule_scenario.test()
+    assert proc.returncode == 0
